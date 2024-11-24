b1d3b936fab942ed9b759787b57635fb9a501a03
Namerid-learn <nkalenik77@gmail.com>
2024-11-24 14:52:05 +0300

    add 1_2.py changed 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
 import random
 
 def print_matrix(matrix):
-        for row in matrix:
-            for el in row:
-                print (f'{el:^3}', end='  ')
-            print ()
+    for row in matrix:
+        for el in row:
+            print (f'{el:^3}', end='  ')
+        print ()
 
 try:    
     while True:
@@ -19,39 +19,27 @@ try:
     matrix = [[random.randint(-15, 15) for i in range(number_of_columns)] for i in range(number_of_rows)]
     original_matrix = matrix[:]
 
-    min = matrix[0][0]
-    row_index_min = 0
-    el_index_min = 0
-    row_index = 0
-    el_index = 0
+    min_ = matrix[0][0]
+    row_index_min_ = 0
+    el_index_min_ = 0
 
-    while row_index != number_of_rows - 1:
-        el_index = 0
-        while el_index != number_of_columns - 1:
-            if matrix[row_index][el_index] < min:
-                min = matrix[row_index][el_index]
-                row_index_min = row_index
-                el_index_min = el_index
-            el_index += 1
-
-        row_index += 1
-
-    matrix.pop(row_index_min)
+    for i in range(number_of_rows):
+        for j in range(number_of_columns):
+            if matrix[i][j] < min_:
+                min_ = matrix[i][j]
+                row_index_min_ = i
+                el_index_min_ = j
 
     print ("\nИсходная матрица:\n")
     print_matrix(original_matrix)
+    print ("\nМинимальный элемент:", min_)
+    print ("\nИндексы минимального элемента:")
+    print ("Столбец:", el_index_min_ + 1)
+    print ("Строка:", row_index_min_ + 1)
 
-    row_index = 0
-    el_index = 0
-
-    while row_index != number_of_rows - 1:
-        el_index = 0
-        while el_index != number_of_columns - 1:
-            if el_index == el_index_min:
-                matrix[row_index].pop(el_index)
-            el_index += 1
-
-        row_index += 1
+    matrix.pop(row_index_min_)
+    for row in matrix:
+        row.pop(el_index_min_)
 
     print ("\nИтоговая матрица:\n")
     print_matrix(matrix)
